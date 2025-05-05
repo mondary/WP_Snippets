db7acacab248b1a5f1b7d19446fc82736cb6cf6b
mondary <clement.mondary@gmail.com>
2025-05-05 11:36:25 +0200

    style: adjust CSS for search drawer and field
    Refactor CSS properties for better alignment and readability. Removed redundant styles and increased font size for better visibility.

--- WP_Snippets/WP_POST - search custom.php
+++ WP_Snippets/WP_POST - search custom.php
@@ -5,33 +5,22 @@ add_action('wp_head', function () {
     /* Nettoyage des couches internes */
     #search-drawer .drawer-inner-wrap,
     #search-drawer .search-form,
-    #search-drawer .wp-block-search__inside-wrapper {
+    #search-drawer .wp-block-search__inside-wrapper
+	    {
         all: unset !important;
         display: flex !important;
         align-items: center;
         justify-content: flex-start;
         width: 100%;
-    }
-
+    }    
     /* Champ de recherche géant, aligné à gauche */
-    #search-drawer .search-field {
+			#search-drawer .search-field {
         all: unset !important;
-        font-size: 10vw !important; /* Taille très grande */
+        font-size: 15vw !important; /* Taille très grande */
         color: #ffffff !important;
-        background: transparent !important;
         width: 100% !important;
         max-width: none !important;
-        caret-color: white;
-        font-weight: 700;
-        text-align: left !important;
     }
-
-    /* Suppression de l'icône de recherche */
-    .kadence-svg-icon.kadence-search-svg {
-        display: none !important;
-    }
-
-
     </style>
     <?php
 });
